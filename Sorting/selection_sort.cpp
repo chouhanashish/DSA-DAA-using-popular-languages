d4edd0b22a19d5abf44ec962f7cecc70dfef4187
ashish chouhan <43749522+chouhanashish@users.noreply.github.com>
2019-10-01 09:11:40 +0530

    Update selection_sort.cpp

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,35 +1,37 @@
-/* C++ Program - Selection Sort */
-		
-#include<iostream.h>
-#include<conio.h>
-void main()
+#include <iostream>
+using namespace std;
+void swap(int *a, int *b)
 {
-	clrscr();
-	int size, arr[50], i, j, temp;
-	cout<<"Enter Array Size : ";
-	cin>>size;
-	cout<<"Enter Array Elements : ";
-	for(i=0; i<size; i++)
-	{
-		cin>>arr[i];
-	}
-	cout<<"Sorting array using selection sort...\n";
-	for(i=0; i<size; i++)
-	{
-		for(j=i+1; j<size; j++)
-		{
-			if(arr[i]>arr[j])
-			{
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-			}
-		}
-	}
-	cout<<"Now the Array after sorting is :\n";
-	for(i=0; i<size; i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	getch();
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+void selectionSort(int array[], int size)
+{
+  for (int step = 0; step < size - 1; step++)
+  {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++)
+    {
+      if (array[i] < array[min_idx])
+        min_idx = i;
+    }
+    swap(&array[min_idx], &array[step]);
+  }
+}
+int main()
+{
+  int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  cout << "Sorted array in Acsending Order:\n";
+  printArray(data, size);
 }
